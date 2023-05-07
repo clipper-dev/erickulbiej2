18c5739bbe18fd9bdb76915dcc9e797f09fc66dd
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2023-05-07 17:26:01 +0200

    Hamburger fix

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -61,7 +61,7 @@ function Navbar({ data }: Props) {
             className={
               "h-[2px] w-[20px] relative bg-black before:absolute before:h-[2px] before:w-[20px] before:bg-black after:absolute after:h-[2px] after:w-[20px] after:bg-black transition-all  before:translate-y-2 after:-translate-y-2 before:transition-all after:transition-all " +
               (sidebar
-                ? " after:translate-y-0 before:-translate-y-0 -rotate-45 before:rotate-90 after:rotate-90"
+                ? " after:translate-y-0 before:translate-y-0 -rotate-45 before:rotate-90 after:rotate-90"
                 : "")
             }
           ></div>
