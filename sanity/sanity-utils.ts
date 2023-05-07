6984e0ca625b6b6b9cd2dfbcba8149597567b89e
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2023-05-07 18:06:54 +0200

    cache

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,14 +1,20 @@
 import { client } from "./lib/client";
 export async function getPosts() {
   const data = await client.fetch(
-    `*[_type == "post"]{_id, publishedAt, bio, title, "categories": categories[]->title, "slug": slug.current, author->{name, "image":image.asset->url}, "mainImage": mainImage.asset->url, readingTime, body}`
+    `*[_type == "post"]{_id, publishedAt, bio, title, "categories": categories[]->title, "slug": slug.current, author->{name, "image":image.asset->url}, "mainImage": mainImage.asset->url, readingTime, body}`,
+    {
+      cache: "no-store",
+    }
   );
   return data;
 }
 
 export async function getPost(slug: any) {
   const data = await client.fetch(
-    `*[_type == "post" && slug.current == "${slug}"][0]{_id, publishedAt, bio, title, "categories": categories[]->title, author->{name, "image":image.asset->url}, "slug": slug.current, "mainImage": mainImage.asset->url, readingTime, body}`
+    `*[_type == "post" && slug.current == "${slug}"][0]{_id, publishedAt, bio, title, "categories": categories[]->title, author->{name, "image":image.asset->url}, "slug": slug.current, "mainImage": mainImage.asset->url, readingTime, body}`,
+    {
+      cache: "no-store",
+    }
   );
   return data;
 }
